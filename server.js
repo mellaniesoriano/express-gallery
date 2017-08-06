c157209a3516611c8f79ec58fd158961e4ab311a
mellaniesoriano <msoriano808@gmail.com>
2017-08-05 15:19:36 -1000

    added gallery route

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const express = require('express');
 const bp = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const db = require('./models');
+const Gallery = db.Gallery;
+const galleryRoute = require('./routes/galleryRoutes.js');
+
+const app = express();
+
+app.use('/', galleryRoute);
+
+const server = app.listen(PORT, () => {
+  db.sequelize.sync();
+  console.log(`Server funning on ${PORT}`);
+});
