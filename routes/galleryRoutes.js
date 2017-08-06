389620c1a548ec96a4366f858b89575ee62db6ab
mellaniesoriano <msoriano808@gmail.com>
2017-08-05 15:35:45 -1000

    getting by id works

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const db = require('../models');
 const Gallery = db.Gallery;
 
+// GET / to view a list of gallery photos
 router.route('/')
 .get((req, res) => {
   Gallery.findAll()
@@ -11,6 +12,40 @@ router.route('/')
       console.log(photos);
       console.log('You got this');
       res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  });
+
+// GET /gallery/:id to see a single gallery photo
+// each gallery photo should include a link to delete this gallery photo
+// each gallery photo should include a link to edit this gallery photo
+router.route('/gallery/:id')
+  .get((req, res) => {
+    Gallery.findById(parseInt(req.params.id))
+      .then((photo) => {
+        console.log(photo);
+        console.log('grabbed photo by id');
+        res.end();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+// POST /gallery to create a new gallery photo
+router.route('/gallery')
+  .post((req, res) => {
+    Gallery.create({
+      author: req.body.author,
+      link: req.body.link,
+      description: req.body.description
+    })
+    .then((data) => {
+      console.log(data);
+      console.log('created new photo');
+      res.end();
     })
     .catch((err) => {
       console.log(err);
