fc3b6571d889d9610e1d9cafe8fa58db98f56f53
mellaniesoriano <msoriano808@gmail.com>
2017-08-05 16:27:52 -1000

    added hbs

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 /* jshint esversion: 6 */
 const express = require('express');
 const bp = require('body-parser');
+const exphbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 const db = require('./models');
 const Gallery = db.Gallery;
 const galleryRoute = require('./routes/galleryRoutes.js');
 
 const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 app.use(bp.urlencoded());
 app.use('/', galleryRoute);
