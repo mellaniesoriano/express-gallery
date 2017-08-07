58371211ddae6fdf0971ef60988e84fc042f094b
mellaniesoriano <msoriano808@gmail.com>
2017-08-06 20:27:18 -1000

    refactored and clean

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -1,110 +1,42 @@
 /* jshint esversion: 6 */
 const express = require('express');
 const router = express.Router();
-const db = require('../models');
-const Gallery = db.Gallery;
+const helpers = require('./routeFunctions/galleryFunctions.js');
 
-// GET /gallery/new to see a "new photo" form
+
 router.route('/gallery/new')
   .get((req, res) => {
-    console.log('getting new?');
     res.render('new');
   });
 
-// GET / to view a list of gallery photos
 router.route('/')
-.get((req, res) => {
-  Gallery.findAll()
-    .then((allPhotos) => {
-      console.log(allPhotos);
-      console.log('You got this');
-      res.render('index', {allPhotos});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  .get((req, res) => {
+    helpers.displayAllPhotos(req, res);
   });
 
-
-// POST /gallery to create a new gallery photo
 router.route('/gallery')
-.get((req, res) => {
-  Gallery.findAll()
-    .then((allPhotos) => {
-      console.log(allPhotos);
-      console.log('You got this');
-      res.render('index', {allPhotos});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  .get((req, res) => {
+    helpers.displayAllPhotos(req, res);
   })
-.post((req, res) => {
-  Gallery.create({
-    author: req.body.author,
-    link: req.body.link,
-    description: req.body.description
-  })
-    .then((data) => {
-      console.log(data);
-      console.log('created new photo');
-      res.redirect('/gallery/');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  .post((req, res) => {
+    helpers.postPhoto(req, res);
   });
 
-// GET /gallery/:id to see a single gallery photo
-// each gallery photo should include a link to delete this gallery photo
-// each gallery photo should include a link to edit this gallery photo
 router.route('/gallery/:id')
-.get((req, res) => {
-  Gallery.findById(parseInt(req.params.id))
-    .then((singlePhoto) => {
-      console.log(singlePhoto.id);
-      console.log('grabbed photo by id');
-      res.render('singlePhoto', {singlePhoto});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  .get((req, res) => {
+    helpers.getById(req, res);
   })
-.put((req, res) => {
-  Gallery.update({
-    author: req.body.author,
-    link: req.body.link,
-    description: req.body.description
-  }, {
-    returning: true,
-    where: {
-      id: req.params.id
-    }
+  .put((req, res) => {
+    helpers.editPhoto(req, res);
   })
-  .then((data) => {
-    console.log(data);
-    console.log('putting!!');
-    res.end();
-  })
-  .catch((err) => {
-    console.log(err);
+  .delete((req, res) => {
+    helpers.deletePhoto(req, res);
   });
-})
-.delete((req, res) => {
-  Gallery.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-  .then((data) => {
-    console.log(data);
-    console.log('deleted!!');
-    res.end();
-  })
-  .catch((err) => {
-    console.log(err);
+
+router.route('/gallery/:id/edit')
+  .get((req, res) => {
+    helpers.editById(req, res);
   });
-});
 
 
 module.exports = router;
