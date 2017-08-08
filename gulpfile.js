86ac4284a97f2d05b8e96caa03a60ebe95da80e5
mellaniesoriano <msoriano808@gmail.com>
2017-08-07 19:56:58 -1000

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('scss/styles.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css/'));
+        .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('scss/styles.scss',['styles']);
 });
