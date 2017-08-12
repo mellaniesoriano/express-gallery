2c0da82b54173f6dfb12881e5d3ab526d6c8b455
mellaniesoriano <msoriano808@gmail.com>
2017-08-11 21:16:11 -1000

    minor changes

--- server.js
+++ server.js
@@ -3,14 +3,75 @@ const express = require('express');
 const bp = require('body-parser');
 const exphbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const db = require('./models');
-const Gallery = db.Gallery;
+const { Gallery, User } = db;
 const galleryRoute = require('./routes/galleryRoutes.js');
+const loginRoute = require('./routes/loginRoutes.js');
 const methodOverride = require('method-override');
 
 const app = express();
 
 app.use(bp.urlencoded());
+
+app.use(session({
+  secret: 'Keyboard Cat'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    console.log('client-side-username', username);
+    console.log('client-side-password', password);
+
+    User.findOne({
+      where : {
+        username : username
+      }
+    }).then((user) => {
+      console.log('user exists in DB');
+      if (user.password === password) {
+        console.log('username and password successful');
+        return done(null, user);
+      } else {
+        console.log('password was incorrect');
+        return done(null, false, { message: 'incorrect password' });
+      }
+    }).catch((err) => {
+      console.log('username not found');
+      console.log(err);
+      return done(null, false, { message: 'incorrect username' });
+    });
+  }
+));
+
+
+passport.serializeUser((user, done) => {
+  console.log('serializing the user into session');
+  done(null, user.id);
+});
+
+passport.deserializeUser((userId, done) => {
+  console.log('adding user info into the req obj');
+  User.find({
+    where: {
+      id: userId
+    }
+  }).then((user) => {
+    return done(null, {
+      id: user.id,
+      username: user.username
+    });
+  }).catch((err) => {
+    done(err, user);
+  });
+});
+
+app.use(express.static('public'));
 
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(methodOverride(function (req, res) {
@@ -29,9 +90,8 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-app.use(express.static('public'));
-
 app.use('/', galleryRoute);
+app.use('/login', loginRoute);
 
 const server = app.listen(PORT, () => {
   db.sequelize.sync();
