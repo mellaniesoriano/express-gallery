c1c1bb12bb0cde60590d56cc53d2d67fa834fedd
mellaniesoriano <msoriano808@gmail.com>
2017-08-11 21:14:55 -1000

    added user

---
+++ models/User.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+
+  return User;
+};
